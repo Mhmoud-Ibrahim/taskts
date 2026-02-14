bb0999a3d6c09a3f55be321228e16bd84269eb4e
Mhmoud-Ibrahim <mahmoudd188@gmail.com>
2026-02-15 01:48:13 +0400

    fff

--- src/middleware/protectedRoute.ts
+++ src/middleware/protectedRoute.ts
@@ -12,9 +12,6 @@ interface MyToken {
 export const protectedRoute = catchError(async( req: Request, res: Response, next: NextFunction)=>{
     let {token} = req.headers
     if(!token) return next(new AppError('token not provided',401))
-    const decoded = jwt.verify(token as string,process.env.JWT_KEY as string )as MyToken
-    let user =await  User.findById(decoded.userId)
-   if(user) (req as any).user  = user
-  if(!user) next(new AppError('user not found',401))
+
     next()
     })
