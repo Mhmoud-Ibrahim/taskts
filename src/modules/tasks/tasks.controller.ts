5fcc2dcdebdde6606abc5532739f75aef125291e
Mhmoud-Ibrahim <mahmoudd188@gmail.com>
2026-02-15 17:07:13 +0400

    dfdfd

--- src/modules/tasks/tasks.controller.ts
+++ src/modules/tasks/tasks.controller.ts
@@ -20,7 +20,7 @@ const addTask=catchError(async (req:any,res:any)=>{
 })
 
 const gettasks= catchError(async (req:any,res:any,next)=>{
-    const userId = req.params.userId
+    const userId = req.body.userId
     const tasks = await Tasks.findOne({user:userId})
     if(!tasks) return next(new AppError('tasks not found',404)) 
     res.json({message:"success",tasks});
