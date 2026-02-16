4e4377ba3a9b9a93bcfffc1cbca30b8f466a93e3
Mhmoud-Ibrahim <mahmoudd188@gmail.com>
2026-02-16 23:29:05 +0400

    fff

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -24,7 +24,7 @@ const signin = catchError(async (req, res, next) => {
         let token = jwt.sign({userId: user._id, email: user.email, name: user.name }, process.env.JWT_KEY as string)
         res.cookie('access_token', token, {
             httpOnly: true,   
-            secure: process.env.MODE === 'production',     
+            secure: true,     
             sameSite: 'strict', 
             maxAge: 3600000    
         })
