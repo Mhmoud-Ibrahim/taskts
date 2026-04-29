0b0ee8d2c6f277e05782745b213539a249cece8d
Mhmoud-Ibrahim <mahmoudd188@gmail.com>
2026-04-29 21:06:19 +0400

    wagha

--- dist/src/modules/user/user.routes.js
+++ dist/src/modules/user/user.routes.js
@@ -1,16 +1,15 @@
-import { Router } from "express";
 import { getMe, logout, signin, signup } from "./user.controller.js";
 import { authenticate } from "../../middleware/authintecate.js";
-import passport from "passport";
+import { Router } from 'express';
+import passport from 'passport';
 import * as authController from './user.controller.js';
 const userRouter = Router();
-userRouter
-    .post('/signup', signup)
-    .post('/signin', signin)
-    .post('/logout', logout)
-    .get('/me', authenticate, getMe);
 userRouter.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
 userRouter.get('/google/callback', passport.authenticate('google', { session: false, failureRedirect: 'https://vercel.app' }), authController.googleAuthSuccess);
+userRouter.post('/signup', signup);
+userRouter.post('/signin', signin);
+userRouter.post('/logout', logout);
+userRouter.get('/me', authenticate, getMe);
 // --- Forgot & Reset Password Routes ---
 userRouter.post('/forgotPassword', authController.forgotPassword); // لإرسال الإيميل
 userRouter.patch('/resetPassword', authController.resetPassword); // لتغيير الباسورد الفعلي
